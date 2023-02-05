0fef7ed220cdfd6bf9bea894ca7d94f980cc61ec
dlg1206 <dlg1206@g.rit.edu>
2023-02-05 17:12:34 -0500

    stub out main

--- Labs/lab1/src/lab1.java
+++ Labs/lab1/src/lab1.java
@@ -5,5 +5,10 @@
 public class lab1 {
     public static void main(String[] args) {
 
+        if(args.length != 4){
+            System.err.println("Incorrect Number of arguments");
+            System.err.println("Expected usage: java lab1 <terrain-image> <elevation-file> <path-file> <output-image-filename>");
+        }
+
     }
 }
