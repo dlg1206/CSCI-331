2ee4e5c83ad89e9c8e27f5556a27ed7b869f9f8d
Derek Garcia <dlg1206@g.rit.edu>
2023-02-05 17:59:36 -0500

    draw on png working

--- Labs/lab1/src/lab1.java
+++ Labs/lab1/src/lab1.java
@@ -1,4 +1,8 @@
-
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.LinkedList;
 
 /**
  * file: lab1.java
@@ -8,6 +12,16 @@
  * @author Derek Garcia
  **/
 public class lab1 {
+
+    private class Coordinate{
+        public int x;
+        public int y;
+
+        public Coordinate(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
     // 1px = 10.29m x 7.55m area
 
     // terrain image to read? (395x500)
@@ -23,11 +37,26 @@ public class lab1 {
 //
 //    }
 
+    private static void drawPath(BufferedImage terrain, LinkedList<Coordinate> path){
+        for(Coordinate c : path)
+            terrain.setRGB(c.x, c.y, Color.red.getRGB());
+    }
+
     public static void main(String[] args) {
         // Check for correct args
         if(args.length != 4){
             System.err.println("Incorrect Number of arguments");
             System.err.println("Expected usage: java lab1 <terrain-image> <elevation-file> <path-file> <output-image-filename>");
+            return;
+        }
+
+        // attempt to load objects
+        try{
+            BufferedImage terrain = ImageIO.read(new File(args[0]));
+            drawPath(terrain, new LinkedList<>());
+            ImageIO.write(terrain, "png", new File(args[3]));
+        } catch (Exception e){
+            System.err.println("Failed to load arguments | Message: " + e.getMessage());
         }
 
     }
