766cf74dcc44bb31355209f5b6568d3085619c11
Derek Garcia <dlg1206@g.rit.edu>
2023-02-05 19:25:01 -0500

    added initial comments

--- Labs/lab1/src/lab1.java
+++ Labs/lab1/src/lab1.java
@@ -10,18 +10,32 @@ import java.util.LinkedList;
 /**
  * file: lab1.java
  *
+ * Find the shortest path for an orienteering course using A* search
+ *
  * CSCI-331 Lab 1
  *
  * @author Derek Garcia
  **/
 public class lab1 {
 
-    private static double[][] ELEVATIONS;
+    private static double[][] ELEVATIONS;   // obj to track elevations
 
+    //
+    // Utility Objects
+    //
+
+    /**
+     * Coordinate class to track coordinates
+     */
     private static class Coordinate{
         public int x;
         public int y;
 
+        /**
+         * Make new Coordinate
+         * @param x x value
+         * @param y y value
+         */
         public Coordinate(int x, int y){
             this.x = x;
             this.y = y;
@@ -33,61 +47,105 @@ public class lab1 {
     }
 
 
-    private static void doAStarSearch(BufferedImage terrain){
+    //
+    // Loaders; load files on startup
+    //
 
-    }
-
-
+    /**
+     * Loads Path File with the list of control points
+     *
+     * @param pathFile file with goal controls
+     * @return LinkedList of goal Coordinate objects
+     * @throws Exception Issue Reading the Path file
+     */
     private static LinkedList<Coordinate> loadGoalCoords(String pathFile) throws Exception {
+        // Attempt to load file
         try{
-            BufferedReader br = new BufferedReader(new FileReader(pathFile));
             LinkedList<Coordinate> coords = new LinkedList<>();
+            BufferedReader br = new BufferedReader(new FileReader(pathFile));   // Open file
+
+            // Add all control points to coord list
             while(br.ready()){
                 String[] points = br.readLine().split(" ");
                 coords.add(new Coordinate(Integer.parseInt(points[0]), Integer.parseInt(points[1])));
             }
+
+            // Close stream and return findings
             br.close();
             return coords;
         } catch (Exception e){
+            // Something went wrong loading the file
             System.err.println("Unable to read file \"" + pathFile + "\" into coordinates");
             throw e;
         }
     }
 
 
+    /**
+     * Loads Elevation File with the list of control points
+     * Hard codes size as per assignment
+     *
+     * @param elevationFile file with all elevations
+     * @return 2D double array with all elevation points
+     * @throws IOException Issue Reading the Elevations file
+     */
     private static double[][] loadElevations(String elevationFile) throws IOException {
         try{
-            System.out.println("Loading Elevations file. . .");
-            BufferedReader br = new BufferedReader(new FileReader(elevationFile));
             double[][] elevations = new double[500][395];     // elevations hardcoded as per assignment
+
+            // Add all elevation points to corresponding point in array
             int row = 0;
+            BufferedReader br = new BufferedReader(new FileReader(elevationFile));
             while(br.ready()){
                 String[] elevation = br.readLine().strip().split("   ");
-
-                // Add each double.
+                // Add each double
                 for(int col = 0; col < 395; col++)
                     elevations[row][col] = Double.parseDouble(elevation[col]);
                 row++;
             }
+            // Close and return object
             br.close();
-            System.out.println("Complete!");
             return elevations;
 
         } catch (Exception e){
+            // Something went wrong loading the file
             System.err.println("Unable to read file \"" + elevationFile + "\" into array");
             throw e;
         }
     }
 
+
+    ///
+    /// Helpers
+    ///
+
+    /**
+     * Helps index elvations as to not get row / column and x and y confused
+     *
+     * @param x x coordinate
+     * @param y y coordinate
+     * @return elevation at the given coordinate
+     */
     private static double getElevationAt(int x, int y){
         return ELEVATIONS[y][x];    // index row column vs x y
     }
+
+
 
     private static void drawPath(BufferedImage terrain, LinkedList<Coordinate> path){
         for(Coordinate c : path)
             terrain.setRGB(c.x, c.y, Color.red.getRGB());
     }
 
+    private static void doAStarSearch(BufferedImage terrain){
+
+    }
+
+    /**
+     * Load all files and perform A* search. Writes an updated map with the path taken
+     *
+     * @param args <terrain-image> <elevation-file> <path-file> <output-image-filename>
+     */
     public static void main(String[] args) {
         // Check for correct args
         if(args.length != 4){
