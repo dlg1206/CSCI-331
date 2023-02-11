374d24f5f9b68af72797dcd0bc4a7886b3a155c5
dlg1206 <dlg1206@g.rit.edu>
2023-02-10 19:32:13 -0500

    Commented lab

--- Labs/lab1/src/lab1.java
+++ Labs/lab1/src/lab1.java
@@ -9,9 +9,7 @@ import java.util.*;
 
 /**
  * file: lab1.java
- *
  * Find the shortest path for an orienteering course using A* search
- *
  * CSCI-331 Lab 1
  *
  * @author Derek Garcia
@@ -19,8 +17,9 @@ import java.util.*;
 public class lab1 {
 
     private static double[][] ELEVATIONS;   // obj to track elevations
-    private static double X_DIST = 10.29;
-    private static double Y_DIST = 7.55;
+    // 1 pxl = 10.29 m x 7.55 m
+    private static final double X_DIST = 10.29;
+    private static final double Y_DIST = 7.55;
 
     //
     // Utility Objects
@@ -32,11 +31,8 @@ public class lab1 {
     private static class Coordinate{
         private final int x;
         private final int y;
-
         private double f;
-
         private final double elevation;
-
         private double totalDistance;   // totalDistance from start
         private Coordinate parent = null;
 
@@ -67,6 +63,12 @@ public class lab1 {
         // Methods
         //
 
+        /**
+         * Calculate distance in meters to other Coordinates
+         *
+         * @param other Target Coordinate
+         * @return distance to other coordinate in meters
+         */
         private double calcDistance(Coordinate other){
             double xDist = Math.pow(this.x - other.x, 2);
             double yDist = Math.pow(this.y - other.y, 2);
@@ -74,8 +76,14 @@ public class lab1 {
             return Math.sqrt( (X_DIST * xDist) + (Y_DIST * yDist) + zDist);
         }
 
+        /**
+         * Get all 8 adjacent successors to current coordinate
+         *
+         * @return 8 adjacent successors
+         */
         public LinkedList<Coordinate> getSuccessors(){
             LinkedList<Coordinate> successors = new LinkedList<>();
+            // Index 8 adjacent ares
             for(int x = -1; x < 2; x++){
                 for(int y = -1; y < 2; y++){
                     // Don't add current not to successors
@@ -86,29 +94,44 @@ public class lab1 {
             return successors;
         }
 
+
+        /**
+         * Compare Coordinates based on their x and y coordinates
+         *
+         * @param obj Other object
+         * @return true if x and y coordinates match, false otherwise
+         */
         @Override
         public boolean equals(Object obj) {
-            if(obj instanceof Coordinate){
-                Coordinate other = (Coordinate) obj;
+            if(obj instanceof Coordinate other){
                 return this.x == other.x && this.y == other.y;
             }
             return false;
         }
 
+        /**
+         * @return (x, y) to string
+         */
         @Override
         public String toString(){
             return "(" + this.x + ", " + this.y + ")";
         }
     }
 
+    /**
+     * Utility Compilation that compares Coordinate f values
+     */
     private static class HeuristicComparator implements Comparator<Coordinate>{
+        /**
+         * @param o1 the first object to be compared.
+         * @param o2 the second object to be compared.
+         * @return compared f value
+         */
         @Override
         public int compare(Coordinate o1, Coordinate o2) {
             return Double.compare(o1.f, o2.f);
         }
     }
-    
-
 
     //
     // Loaders; load files on startup
@@ -124,18 +147,18 @@ public class lab1 {
     private static LinkedList<Coordinate> loadGoalCoords(String pathFile) throws Exception {
         // Attempt to load file
         try{
-            LinkedList<Coordinate> coords = new LinkedList<>();
+            LinkedList<Coordinate> coordinates = new LinkedList<>();
             BufferedReader br = new BufferedReader(new FileReader(pathFile));   // Open file
 
-            // Add all control points to coord list
+            // Add all control points to coordinate list
             while(br.ready()){
                 String[] points = br.readLine().split(" ");
-                coords.add(new Coordinate(Integer.parseInt(points[0]), Integer.parseInt(points[1])));
+                coordinates.add(new Coordinate(Integer.parseInt(points[0]), Integer.parseInt(points[1])));
             }
 
             // Close stream and return findings
             br.close();
-            return coords;
+            return coordinates;
         } catch (Exception e){
             // Something went wrong loading the file
             System.err.println("Unable to read file \"" + pathFile + "\" into coordinates");
@@ -194,41 +217,46 @@ public class lab1 {
     }
 
 
-    private static boolean containsBetterSuccessor(LinkedList<Coordinate> list, Coordinate successor){
-        if(list.contains(successor)){
-            Coordinate other = list.get(list.indexOf(successor));
-            return other.f < successor.f;
-        }
-
-        return false;   // list doesn't contain successor
-    }
-
+    /**
+     * Draws path on terrain image between control points
+     *
+     * @param terrain terrain png to draw path on
+     * @param goal Goal Coordinate to start at
+     */
     private static void drawPath(BufferedImage terrain, Coordinate goal){
+        // Start at goal and move backwards until reach previous control point
         while(goal != null){
             terrain.setRGB(goal.x, goal.y, Color.red.getRGB());
             goal = goal.parent;
         }
-
     }
 
+    /**
+     * Calculate terrain cost based on given rgb code
+     *
+     * @param rgb rgb code of current terrain
+     * @return terrain cost
+     */
     private static int getTerrainCost(int rgb){
+        // Convert to hex
         String buf = Integer.toHexString(rgb);
-        String hex = "#" + buf.substring(buf.length()-6).toUpperCase();
+        String hex = "#" + buf.substring(buf.length() - 6).toUpperCase();
+        // Get terrain cost
         return switch (hex) {
             // open land
             case "#F89412" -> 3;
             // rough meadow
-            case "#FFC000" -> 4;
+            case "#FFC000" -> 5;
             // Easy forest movement
-            case "#FFFFFF" -> 5;
+            case "#FFFFFF" -> 6;
             // Slow run forest
-            case "#02D03C" -> 7;
+            case "#02D03C" -> 8;
             // Walk forest
-            case "#028828" -> 6;
+            case "#028828" -> 7;
             // Impassible vegetation
             case "#054918" -> Integer.MAX_VALUE;
             // Lake/Swamp/Marsh
-            case "#0000FF" -> 8;
+            case "#0000FF" -> 20;
             // Paved road
             case "#473303" -> 1;
             // Footpath
@@ -236,18 +264,23 @@ public class lab1 {
             // Out of bounds
             case "#CD0066" -> Integer.MAX_VALUE;
 
+            // Default to illegal area
             default -> Integer.MAX_VALUE;
         };
-
-
     }
 
 
-
-    private static Coordinate doAStarSearch(BufferedImage terrain, Coordinate source, Coordinate sink) throws Exception {
+    /**
+     * Perform A* search
+     *
+     * @param terrain Terrain to reference
+     * @param source Starting coordinate
+     * @param sink Ending coordinate
+     * @return Ending coordinate with path back to start
+     */
+    private static Coordinate doAStarSearch(BufferedImage terrain, Coordinate source, Coordinate sink){
         // timer for metrics
         long start = System.currentTimeMillis();
-        long end = start + 20 * 1000;
 
         // init queues
         TreeSet<Coordinate> frontier = new TreeSet<>(new HeuristicComparator());
@@ -259,11 +292,6 @@ public class lab1 {
 
         // Repeat until nothing is left in the frontier
         while( !frontier.isEmpty() ){
-            if(System.currentTimeMillis() > end){
-                throw new Exception("Exceeded time limit");
-            }
-
-
             Coordinate parent = frontier.pollFirst();
 
             // Go through all current coordinate's successor
@@ -272,29 +300,24 @@ public class lab1 {
 
                 // if find goal, return Coordinate with path info
                 if(successor.equals(sink)){
-//                    print(terrain, frontier, explored,source,sink);
+                    // Report metrics
                     System.out.println("\t" + source + " -> " + sink + " | Time Elapsed: " + ((System.currentTimeMillis() - start) / (double) 1000) + " seconds");
                     return successor;
                 }
 
+                // Get cost and make sure not infeasible
                 int terrainCost = getTerrainCost(terrain.getRGB(successor.x, successor.y));
-                // inValid
                 if(terrainCost == Integer.MAX_VALUE)
                     continue;
 
-
-
+                // Calculate total distance
                 successor.totalDistance = terrainCost + successor.calcDistance(parent) + parent.totalDistance;
 
-//                System.out.println("Calculating Heuristics for: " + successor);
-//                successor.f = successor.totalDistance + getHeuristic(terrain, successor, sink);
-
+                // Update f value with heuristic
                 successor.f = successor.totalDistance + successor.calcDistance(sink);
-                // Check if better successor exists
-//                if(containsBetterSuccessor(explored, successor))
-//                   continue;
-                boolean cont = false;
 
+                // Check if better successor exists
+                boolean cont = false;
                 for(Coordinate c : frontier){
                     if(successor.equals(c) && c.f < successor.f){
                         cont = true;
@@ -312,22 +335,23 @@ public class lab1 {
                 if (cont)
                     continue;
 
-
-                    frontier.add(successor);
+                // No better successor, so add to frontier
+                frontier.add(successor);
             }
-            explored.push(parent);
+            explored.push(parent);  // add current to explored
         }
 
         // No path was found :(
         return null;
     }
 
+
     /**
      * Load all files and perform A* search. Writes an updated map with the path taken
      *
      * @param args <terrain-image> <elevation-file> <path-file> <output-image-filename>
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         // Check for correct args
         if(args.length != 4){
             System.err.println("Incorrect Number of arguments");
@@ -342,42 +366,31 @@ public class lab1 {
             terrain = ImageIO.read(new File(args[0]));
             ELEVATIONS = loadElevations(args[1]);
             goals = loadGoalCoords(args[2]);
-            // Repeat until only goal is left
-
         } catch (Exception e){
             System.err.println("Failed to load arguments | Message: " + e.getMessage());
             return;
         }
 
         try {
-            LinkedList<Coordinate> goals2 = loadGoalCoords(args[2]);
             System.out.println("Total Control Points: " + goals.size());
             long start = System.currentTimeMillis();
+
+            // Repeat until only goal is left
             while(goals.size() != 1){
                 Coordinate goal = doAStarSearch(terrain, goals.pop(), goals.peek());
                 drawPath(terrain, goal);
             }
-            System.out.println("Total Time Elapsed: " + ((System.currentTimeMillis() - start) / (double) 1000));
-            for (Coordinate c : goals2){
-                terrain.setRGB(c.x, c.y, Color.blue.getRGB());
-            }
-        } catch (Exception e){
-            System.err.println(e);
-            ImageIO.write(terrain, "png", new File(args[3]));
+            System.out.println("Total Time Elapsed: " + ((System.currentTimeMillis() - start) / (double) 1000));    // time for metrics
 
+        } catch (Exception e){
+            System.err.println(e.getMessage());
         }
 
-
-
-        
-        
         // Attempt to write final image
         try {
             ImageIO.write(terrain, "png", new File(args[3]));
         } catch (Exception e){
             System.err.println("Unable to write to file \"" + args[3] + "\"| Message: " + e.getMessage());
         }
-        
-
     }
 }
