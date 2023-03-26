5a84b0eded8c22aa8420f203ee1a70f9983ee85b
dlg1206 <dlg1206@g.rit.edu>
2023-03-26 17:27:47 -0400

    init main

---
+++ Labs/lab2/src/Lab2.java
@@ -0,0 +1,20 @@
+/**
+ * file: lab2.java
+ * CSCI-331 Lab 2
+ *
+ * @author Derek Garcia
+ **/
+public class Lab2 {
+
+
+
+    public static void main(String[] args) {
+        // Check for correct args
+        if(args.length != 1){
+            System.err.println("Incorrect Number of Arguments");
+            System.err.println("Expected usage: java lab2 <path-to-KB.cnf>");
+            return;
+        }
+
+    }
+}
