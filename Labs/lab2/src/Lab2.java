4f92038f9df28d3b9ae1dcc140e5755a97c0acb2
dlg1206 <dlg1206@g.rit.edu>
2023-04-02 01:33:05 -0400

    stub out helper objects

--- Labs/lab2/src/Lab2.java
+++ Labs/lab2/src/Lab2.java
@@ -6,11 +6,35 @@ import java.util.List;
 
 public class Lab2 {
 
+    // helper classes
+    private class Clause{
+
+    }
+    private class Predicate{
+
+    }
+    private class term{
+
+    }
+
+    public static List<Clause> loadFile(String filePath){
+        return null;
+    }
+
     public static void main(String[] args) {
         // Check for correct args
         if(args.length != 1){
             System.err.println("Incorrect Number of Arguments");
             System.err.println("Expected usage: java lab2 <path-to-KB.cnf>");
+            return;
+        }
+
+        List<Clause> clauses;
+        try{
+            clauses = loadFile(args[0]);
+        } catch (Exception e){
+            System.err.println("Failed to load Knowledge Base \"" + args[0] + "\"");
+            System.err.println("Error: " + e);
             return;
         }
 
