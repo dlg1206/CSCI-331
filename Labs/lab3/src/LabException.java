327b8afaa4d16c283be8e241e505999e58902e9f
dlg1206 <dlg1206@g.rit.edu>
2023-04-14 19:29:06 -0400

    commented labexception

--- Labs/lab3/src/LabException.java
+++ Labs/lab3/src/LabException.java
@@ -1,15 +1,24 @@
 /**
+ * file: LabException.java
+ *
+ * Utility exceptions for parsing data files
  * @author Derek Garcia
  **/
-
 public class LabException {
 
+    /**
+     * Input Arguments were invalid
+     */
     public static class BadArgs extends Exception{
         public BadArgs(String msg){
             super(msg);
         }
     }
 
+
+    /**
+     * Unable to read in data file
+     */
     public static class BadDatFile extends Exception{
         public BadDatFile(String msg){
             super(msg);
